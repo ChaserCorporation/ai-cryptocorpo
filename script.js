9bdee8acd064ca21060e3a932444353f0edca712
ChaserCorporation <sinistrart@gmail.com>
2025-03-30 19:45:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// Gestion du mode sombre/clair
-document.getElementById('theme-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('light-mode');
-    const icon = document.getElementById('theme-toggle');
-    icon.textContent = document.body.classList.contains('light-mode') ? '🌙' : '🌞';
-});
-
 // Gestion de l'ouverture/fermeture du menu latéral
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('.sidebar').classList.toggle('open');
